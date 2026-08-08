dc3a4f43fe9d9a56a1acec8bc8d3983cf58d3d09
hafsanuktar011-crypto <hafsamuktar011@gmail.com>
2026-08-08 03:55:48 +0300

    style: center hero search box

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -35,17 +35,17 @@ function Hero() {
                 <div className="flex items-center space-x-2 relative">
                   <IoLocationSharp  className='absolute ml-45'/>
                   <input type="text" placeholder='Location' 
-                  className='bg-amber-50 py-1.5 px-3 rounded-md'/>
+                  className='bg-[#F8EEEC] py-1.5 px-3 rounded-md'/>
                 </div>
                 <div className="flex items-center space-x-2">
                   <AiTwotoneHome className='absolute ml-45' />
                   <input type="text" placeholder='Type' 
-                    className='bg-amber-50 py-1.5 px-3 rounded-md'/>
+                    className='bg-[#F8EEEC] py-1.5 px-3 rounded-md'/>
                 </div>
                 <div className="flex items-center space-x-2">
                   <TbCreativeCommonsNc className='absolute ml-45' />
                   <input type="text" placeholder='Category'
-                    className='bg-amber-50 py-1.5 px-3 rounded-md' />
+                    className='bg-[#F8EEEC] py-1.5 px-3 rounded-md' />
                 </div>
                 <button className=" text-white px-3 py-1 rounded">Sign Up</button>
               </div>
